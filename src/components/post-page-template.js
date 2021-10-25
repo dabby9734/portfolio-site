1180f8af6eb8f18310ea0332c5d95ec44f06c209
dabby9734 <weichufmsp@gmail.com>
2021-10-25 22:22:32 +0800

    Blog posts styled with basic css

--- src/components/post-page-template.js
+++ src/components/post-page-template.js
@@ -1,6 +1,8 @@
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { Helmet } from 'react-helmet';
 import React from 'react';
+import '../style/main.scss';
 
 export const query = graphql`
   query PostsByID($id: String!) {
@@ -8,6 +10,8 @@ export const query = graphql`
       body
       frontmatter {
         title
+        description
+        author
         date(formatString: "YYYY MMMM Do")
       }
     }
@@ -17,10 +21,28 @@ export const query = graphql`
 export default ({ data }) => {
   const { frontmatter, body } = data.mdx;
   return (
-    <div>
-      <h1>{frontmatter.title}</h1>
-      <p>{frontmatter.date}</p>
-      <MDXRenderer>{body}</MDXRenderer>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{frontmatter.title}</title>
+        <html lang="en" />
+        <meta name="description" content={frontmatter.description} />
+      </Helmet>
+      <section id="blog">
+        <div className="blog-post-wrapper">
+          <div className="blog-post-wrapper__title">
+            <h1>{frontmatter.title}</h1>
+            <p>
+              {`Posted By `}
+              <em>{frontmatter.author}</em>
+              {` on ${frontmatter.date}`}
+            </p>
+          </div>
+          <div className="blog-post-wrapper__content">
+            <MDXRenderer>{body}</MDXRenderer>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
