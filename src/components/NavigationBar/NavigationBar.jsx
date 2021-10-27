9346168bafa5b8073cd4ebfb39df4aecdd2c52cf
dabby9734 <weichufmsp@gmail.com>
2021-10-27 21:30:58 +0800

    Add back navbar to fix capitalisation issue

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+// import logo from '../../images/dabby_teal_square.jpg';
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="footer-scroll-up-target">
+      <Container>
+        {/* <Navbar.Brand href="#home">
+          <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />{' '}
+          dabby
+        </Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/blog-homepage">Blog</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
