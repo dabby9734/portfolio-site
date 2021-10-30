5300cafda92bbdabe4e82bef368c3a7661c144b5
dabby9734 <weichufmsp@gmail.com>
2021-10-30 11:59:37 +0800

    more dark theme styling

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -7,23 +7,16 @@ const NavigationBar = () => {
   return (
     <div>
       <Fade top>
-        <Navbar className="footer-scroll-up-target navbar-custom">
+        <Navbar expand="lg" className="footer-scroll-up-target navbar-custom">
           <Container>
             <Navbar.Brand>
               <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/blog-homepage">Blog</Nav.Link>
-                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
               </Nav>
             </Navbar.Collapse>
           </Container>
