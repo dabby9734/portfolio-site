7713f0cce74bc74e621b3a66bbaf5214a40cf7e3
dabby9734 <weichufmsp@gmail.com>
2021-10-31 17:15:24 +0800

    Implemented a collapsible navbar for small screens

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -12,7 +12,7 @@ const NavigationBar = () => {
             <Navbar.Brand>
               <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />
             </Navbar.Brand>
-            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
