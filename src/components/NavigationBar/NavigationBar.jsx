207ed677898f7ad43cfd92a0ad339df5a42489a2
dabby9734 <weichufmsp@gmail.com>
2021-11-08 00:00:13 +0800

    ☀️Implement a dark/light mode toggle icon into navigation bar! 🌙

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
+import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 import logo from '../../images/favicon.png';
 
 const NavigationBar = () => {
   return (
     <div>
       <Fade top>
-        <Navbar expand="lg" className="footer-scroll-up-target navbar-custom">
+        <Navbar expand="sm" className="footer-scroll-up-target navbar-custom">
           <Container>
             <Navbar.Brand>
               <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />
@@ -18,6 +19,26 @@ const NavigationBar = () => {
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/blog-homepage">Blog</Nav.Link>
               </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <ThemeToggler>
+                  {({ theme, toggleTheme }) => (
+                    <div className="theme-toggler-wrapper">
+                      <label htmlFor="checkbox" className="theme-toggler-icons">
+                        <input
+                          type="checkbox"
+                          id="checkbox"
+                          onChange={(e) => toggleTheme(e.target.checked ? 'dark' : 'light')}
+                          checked={theme === 'dark'}
+                        />
+                        <span className="theme-icon-sun">☀️</span>
+                        <span className="theme-icon-moon">🌙</span>
+                      </label>
+                    </div>
+                  )}
+                </ThemeToggler>
+              </Navbar.Text>
             </Navbar.Collapse>
           </Container>
         </Navbar>
